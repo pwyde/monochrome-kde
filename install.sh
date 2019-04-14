14dc1e444663f09cb3e2d147839efdd9eba22a0c
Patrik Wyde <patrik@wyde.se>
2019-04-15 01:04:42 +0200

    Added double dash (--) fucntionality to options.

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Install script for Monochrome KDE
 # Copyright (C) 2019 Patrik Wyde <path@wyde.se>
@@ -27,7 +27,7 @@ uninstall="false"
 temp_file="$(mktemp -u)"
 temp_dir="$(mktemp -d)"
 
-print_header() {
+_print_header() {
 echo "                                             
    _____                 _                        _____ ____  _____ 
   |     |___ ___ ___ ___| |_ ___ ___ _____ ___   |  |  |    \|   __|
@@ -39,7 +39,7 @@ echo "
 " >&2
 }
 
-print_help() {
+_print_help() {
 echo "
 Description:
   Install script for the ${git_desc} theme.
@@ -51,59 +51,62 @@ Examples:
   Uninstall: ${0} -u
  
 Options:
-  -i    Install theme in default location.
+  -i, --install      Install theme in default location.
  
-  -u    Uninstall theme.
+  -u, --uninstall    Uninstall theme.
 " >&2
 }
+
+# Print help if no argument is specified.
+if [[ "${#}" -le 0 ]]; then
+    _print_header
+    _print_help
+    exit 1
+fi
  
-while getopts ":i :u :f: :h" opt; do
-    case $opt in
-        i)
-            install="true"
-        ;;
-        u)
-            uninstall="true"
-        ;;
-        h)
-            print_header
-            print_help
-        exit 1
-        ;;
-        \?)
-            echo "Invalid option '-${OPTARG}'." >&2
-            exit 1
-        ;;
-        *)
-            # Unknown option.
-        ;;
+# Loop as long as there is at least one more argument.
+while [[ "${#}" -gt 0 ]]; do
+    arg="${1}"
+    case "${arg}" in
+        # This is an arg value type option. Will catch both '-i' or
+        # '--install' value.
+        -i|--install) install="true" ;;
+        # This is an arg value type option. Will catch both '-u' or
+        # '--uninstall' value.
+        -u|--uninstall) uninstall="true" ;;
+        # This is an arg value type option. Will catch both '-h' or
+        # '--help' value.
+        -h|--help) _print_header; _print_help; exit 1 ;;
+        *) echo "Invalid option '${arg}'." >&2; _print_header; _print_help; exit 1 ;;
     esac
+    # Shift after checking all the cases to get the next option.
+    shift
 done
 
-print_msg() {
+_print_msg() {
     echo "=>" "${@}" >&2
 }
 
 # Delete parent directories if empty.
-delete_dir() {
+_delete_dir() {
     sudo rm -rf "${1}"
     sudo rmdir -p "$(dirname "${1}")" 2>/dev/null || true
 }
 
-cleanup() {
+_cleanup() {
     rm -rf "${temp_file}" "${temp_dir}"
-    print_msg "Completed!"
+    _print_msg "Completed!"
 }
 
-download_pkg() {
-    print_msg "Downloading latest version from master branch ..."
+_download_pkg() {
+    _print_msg "Downloading latest version from master branch..."
     wget -O "${temp_file}" "https://gitlab.com/pwyde/${git_repo}/-/archive/${tag}/${git_repo}-${tag}.tar.gz"
-    print_msg "Extracting archive ..."
+    _print_msg "Extracting archive..."
     tar -xzf "${temp_file}" -C "${temp_dir}"
 }
 
-install_pkg() {
-    print_msg "Installing ${git_desc} to '${prefix}' ..."
+_install_pkg() {
+    _print_msg "Installing ${git_desc} to '${prefix}'..."
     sudo cp -R \
         "${temp_dir}/${git_repo}-${tag}/aurorae" \
         "${temp_dir}/${git_repo}-${tag}/color-schemes" \
@@ -115,31 +118,32 @@ install_pkg() {
         "${prefix}"
 }
 
-uninstall_pkg() {
-    print_msg "Uninstalling ${git_desc} ..."
-    delete_dir "${prefix}/aurorae/themes/Monochrome"
-    delete_dir "${prefix}/color-schemes/Monochrome.colors"
-    delete_dir "${prefix}/konsole/Monochrome.colorscheme"
-    delete_dir "${prefix}/Kvantum/Monochrome"
-    delete_dir "${prefix}/plasma/desktoptheme/Monochrome"
-    delete_dir "${prefix}/plasma/look-and-feel/com.gitlab.pwyde.monochrome-kde"
-    delete_dir "${prefix}/sddm/themes/monochrome"
-    delete_dir "${prefix}/yakuake/skins/monochrome"
+_uninstall_pkg() {
+    _print_msg "Uninstalling ${git_desc}..."
+    _delete_dir "${prefix}/aurorae/themes/Monochrome"
+    _delete_dir "${prefix}/color-schemes/Monochrome.colors"
+    _delete_dir "${prefix}/konsole/Monochrome.colorscheme"
+    _delete_dir "${prefix}/Kvantum/Monochrome"
+    _delete_dir "${prefix}/plasma/desktoptheme/Monochrome"
+    _delete_dir "${prefix}/plasma/look-and-feel/com.gitlab.pwyde.monochrome-kde"
+    _delete_dir "${prefix}/sddm/themes/monochrome"
+    _delete_dir "${prefix}/yakuake/skins/monochrome"
 }
 
 if [[ "${uninstall}" = "false" && "${install}" = "true" ]]; then
-    print_header
-    download_pkg
-    uninstall_pkg
-    install_pkg
-    cleanup
+    _print_header
+    _download_pkg
+    _uninstall_pkg
+    _install_pkg
+    _cleanup
 elif [[ "${uninstall}" = "true" && "${install}" = "false" ]]; then
-    print_header
-    download_pkg
-    uninstall_pkg
-    cleanup
+    _print_header
+    _download_pkg
+    _uninstall_pkg
+    _cleanup
 else
-    print_msg "Missing or invalid options, see help below."
-    print_header
-    print_help
+    _print_msg "Missing or invalid options, see help below."
+    _print_header
+    _print_help
+    exit 1
 fi
