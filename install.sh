b0ab4767d6d2d005bf636c34febfcbbd14d94688
Patrik Wyde <patrik@wyde.se>
2020-02-26 23:12:48 +0100

    Changed branch from 'master' to 'dev'.

--- install.sh
+++ install.sh
@@ -22,7 +22,7 @@ git_hosting="gitlab.com"
 git_repo="monochrome-kde"
 git_desc="Monochrome KDE"
 prefix="/usr/share"
-tag="master"
+tag="dev"
 install="false"
 uninstall="false"
 ## Colorize output.
